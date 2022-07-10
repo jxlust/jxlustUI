499d2c02a56df80c2f21df7228b8f3aa8513dd93
jxlust <jxljxufe@163.com>
2022-07-10 21:38:53 +0800

    feat: use mydialog plugin

---
+++ examples/views/DialogTest/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <section>
+    <header> openDialog </header>
+    <button @click="openDialog">Open Dialog</button>
+  </section>
+</template>
+
+<script setup lang="ts">
+  import MyDialog from '@pk/MyDialog/useMyDialog';
+  let myDialog = new MyDialog({
+    msg: 'hhh',
+    type: 1,
+    sure: (data: any) => {
+      console.log('sure data:', data);
+    },
+  });
+  const openDialog = () => {
+    myDialog.open();
+  };
+</script>
+
+<style></style>
